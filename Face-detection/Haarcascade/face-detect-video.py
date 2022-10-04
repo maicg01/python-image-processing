4d137c10281ee4403e81d817df214e654966a088
maicg01 <nguyenmaicg03@gmail.com>
2022-10-04 11:35:33 +0700

    Haarcascade_faceDetectVideo

---
+++ Face-detection/Haarcascade/face-detect-video.py
@@ -0,0 +1,47 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+# cap = cv2.VideoCapture(0)
+# To use a video file as input 
+video = cv2.VideoCapture('/home/devai01/Me/python-image-processing/people_check.mp4')
+
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+
+size = (frame_width, frame_height)
+
+result = cv2.VideoWriter('filename.avi', 
+                         cv2.VideoWriter_fourcc(*'MJPG'),
+                         10, size)
+
+
+
+while True:
+    # Read the frame
+    _, img = video.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    
+    # # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+# Release the VideoCapture object
+video.release()
+result.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
+   
+print("The video was successfully saved")
