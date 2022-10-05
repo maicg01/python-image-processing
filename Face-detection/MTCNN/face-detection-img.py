9fc5700ce4c98a58e4d9ba08d5d7a003f1d8b187
maicg01 <nguyenmaicg03@gmail.com>
2022-10-05 08:01:10 +0700

    My message

--- Face-detection/MTCNN/face-detection-img.py
+++ Face-detection/MTCNN/face-detection-img.py
@@ -5,18 +5,19 @@ import cv2
 img = cv2.cvtColor(cv2.imread("/home/devai01/Me/python-image-processing/people.jpg"), cv2.COLOR_BGR2RGB)
 detector = MTCNN()
 
-detector.detect_faces(img)
-[
-    {
-        'box': [277, 90, 48, 63],
-        'keypoints':
-        {
-            'nose': (303, 131),
-            'mouth_right': (313, 141),
-            'right_eye': (314, 114),
-            'left_eye': (291, 117),
-            'mouth_left': (296, 143)
-        },
-        'confidence': 0.99851983785629272
-    }
-]
+faces = detector.detect_faces(img)
+print(len(faces))
+
+# vẽ keypoints và bounding boxes
+def draw(image, face):
+    for i in len(face):
+        person = face[i]
+        x, y, width, height = person['box']
+        cv2.rectangle(image, (x, y), (x+width, y+height), (0, 155, 255), 2)
+
+        for key, value in person['keypoints'].items():
+            cv2.circle(image, value, 2, (0, 155, 255), 2)
+
+    cv2.imwrite('img.jpg', image)
+
+draw(img,faces)
