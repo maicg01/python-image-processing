e09dadd52d4f208684de4d50dd6f3ea18cb3abbf
maicg01 <nguyenmaicg03@gmail.com>
2022-10-05 08:41:05 +0700

    fix pull

--- Face-detection/MTCNN/face-detection-img.py
+++ Face-detection/MTCNN/face-detection-img.py
@@ -2,15 +2,10 @@ import imp
 from mtcnn import MTCNN
 import cv2
 
-img = cv2.cvtColor(cv2.imread("E:\python-image-processing\people.jpg"), cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(cv2.imread("/home/devai01/Me/python-image-processing/people.jpg"), cv2.COLOR_BGR2RGB)
 detector = MTCNN()
 
 faces = detector.detect_faces(img)
-print(len(faces))
-
-# vẽ keypoints và bounding boxes
-def draw(image, face):
-    for i in len(face):
 # [
 #     {
 #         'box': [277, 90, 48, 63],
@@ -32,15 +27,9 @@ def draw(image, face):
         x, y, width, height = person['box']
         cv2.rectangle(image, (x, y), (x+width, y+height), (0, 155, 255), 2)
 
-        for key, value in person['keypoints'].items():
-            cv2.circle(image, value, 2, (0, 155, 255), 2)
-
-    cv2.imwrite('img.jpg', image)
-
-draw(img,faces)
         # for key, value in person['keypoints'].items():
         #     cv2.circle(image, value, 2, (0, 155, 255), 2)
-    
+
     cv2.imwrite("img.jpg", image)
 
 
