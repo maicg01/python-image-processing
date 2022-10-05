7ad8716f8d9df2f1bcc16a36f276901a0ce371a2
maicg01 <nguyenmaicg03@gmail.com>
2022-10-05 08:28:22 +0700

    flix conflic test

--- Face-detection/MTCNN/face-detection-img.py
+++ Face-detection/MTCNN/face-detection-img.py
@@ -6,13 +6,11 @@ img = cv2.cvtColor(cv2.imread("E:\python-image-processing\people.jpg"), cv2.COLO
 detector = MTCNN()
 
 faces = detector.detect_faces(img)
-<<<<<<< HEAD
 print(len(faces))
 
 # vẽ keypoints và bounding boxes
 def draw(image, face):
     for i in len(face):
-=======
 # [
 #     {
 #         'box': [277, 90, 48, 63],
@@ -30,19 +28,16 @@ def draw(image, face):
 
 def draw(image, face):
     for i in range(len(face)):
->>>>>>> d4efba6297a13f2e8552231d6fcebf29c3974697
         person = face[i]
         x, y, width, height = person['box']
         cv2.rectangle(image, (x, y), (x+width, y+height), (0, 155, 255), 2)
 
-<<<<<<< HEAD
         for key, value in person['keypoints'].items():
             cv2.circle(image, value, 2, (0, 155, 255), 2)
 
     cv2.imwrite('img.jpg', image)
 
 draw(img,faces)
-=======
         # for key, value in person['keypoints'].items():
         #     cv2.circle(image, value, 2, (0, 155, 255), 2)
     
@@ -50,4 +45,3 @@ draw(img,faces)
 
 
 draw(img, faces)
->>>>>>> d4efba6297a13f2e8552231d6fcebf29c3974697
