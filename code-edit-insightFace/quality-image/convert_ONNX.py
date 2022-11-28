4ed9bcebc6d8bb971fb4005fb4a09992e4723119
maicg01 <nguyenmaicg03@gmail.com>
2022-11-28 13:52:25 +0700

    convert model pytorch to onnx

---
+++ code-edit-insightFace/quality-image/convert_ONNX.py
@@ -0,0 +1,43 @@
+#convert to onnx runtime
+import torch.onnx 
+from torch.autograd import Variable
+from models.model_resnet import ResNet, FaceQuality
+
+#Function to Convert to ONNX 
+def Convert_ONNX(model): 
+
+    # set the model to inference mode 
+    model.eval() 
+
+    # Let's create a dummy input tensor  
+    dummy_input = Variable(torch.randn(1, 3, 112, 112))
+
+    # Export the model   
+    torch.onnx.export(model,         # model being run 
+         dummy_input,       # model input (or a tuple for multiple inputs) 
+         "./onnx/ImageClassifier.onnx",       # where to save the model  
+         export_params=True,  # store the trained parameter weights inside the model file 
+         opset_version=10,    # the ONNX version to export the model to 
+         do_constant_folding=True,  # whether to execute constant folding for optimization 
+         input_names = ['modelInput'],   # the model's input names 
+         output_names = ['modelOutput'], # the model's output names 
+         dynamic_axes={'modelInput' : {0 : 'batch_size'},    # variable length axes 
+                                'modelOutput' : {0 : 'batch_size'}}) 
+    print(" ") 
+    print('Model has been converted to ONNX') 
+
+def main():
+
+    # Let's load the model we just created and test the accuracy per label 
+    model = ResNet(num_layers=100, feature_dim=512)
+    print("doneeeee")
+
+    # Test with batch of images 
+    #testBatch() 
+    # Test how the classes performed 
+    #testClassess() 
+ 
+    # Conversion to ONNX 
+    Convert_ONNX(model) 
+
+main()
