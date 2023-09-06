962db2f4132f7eed6f9309522986e5037fe17721
maicg01 <nguyenmaicg03@gmail.com>
2023-09-06 15:17:05 +0700

    MaiCG: convert name in file .npy of faiss

---
+++ YOLO/convert_name_npy.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# Đường dẫn đến file .npy ban đầu
+input_file_path = "/home/maicg/Documents/Me/python-image-processing/YOLO/ten_vi_converted.npy"
+
+# Đọc dữ liệu từ file .npy
+names = np.load(input_file_path)
+
+# Chuyển đổi tên
+converted_names = []
+for name in names:
+    words = name.split("-")
+    converted_words = [word.capitalize() for word in words]
+    converted_name = "".join(converted_words)
+    # converted_name = "D/c " + name
+    print(converted_name)
+    converted_names.append(converted_name)
+
+# Đường dẫn đến file .npy mới
+output_file_path = "ten_vi_converted_new.npy"
+
+# Lưu danh sách tên đã chuyển đổi vào file .npy mới
+np.save(output_file_path, converted_names)
+
+# In thông báo khi hoàn thành
+print("Đã lưu danh sách tên đã chuyển đổi vào file .npy mới.")
