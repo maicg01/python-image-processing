532f8546281ebe57818249826aabdbf0772c4559
maicg01 <nguyenmaicg03@gmail.com>
2023-12-01 16:04:18 +0700

    icon hay hay

---
+++ align_image/test.py
@@ -0,0 +1,29 @@
+class myRelationship:
+    def __init__(self):
+        self.content = '🌸🌸🌸🌸🌸'
+class mySelf:
+    def __init__(self):
+        self.content = '❤️❤️❤️❤️❤️'
+class myFamily:
+    def __init__(self):
+        self.content = '🌸🌸🌸🌸🌸'
+class myTeachers:
+    def __init__(self):
+        self.content = '🌸🌸🌸🌸🌸'
+class myFriends:
+    def __init__(self):
+        self.content = '🙆‍♀️🙆‍♀️🙆‍♀️🙆‍♀️'
+class myDestiny:
+    def __init__(self):
+        self.content = "\u2705 \u2764 \U0001F600 \u2765 \u2766 \u2767"
+def sendLove():
+    print('🌸🌸🌸🌸🌸')
+    print(mySelf().content)
+    print(myRelationship().content)
+    print(myFamily().content)
+    print(myTeachers().content)
+    print(myFriends().content)
+    print(myDestiny().content)
+    print('Cảm ơn vì tất cả!🍀🍀')
+          
+sendLove()
